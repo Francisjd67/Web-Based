6080a0f61bc97e1638fd719fc5f6516bcbdef459
Francis Danaher <francisjd67@gmail.com>
2026-04-05 21:45:55 -0400

    final classwork

--- multiplier.js
+++
@@ -1,7 +0,0 @@
-function calculate(){
-let num1 =   document.getElementById("num1").value;
-let num2 = document.getElementById("num2").value;
-let answer = document.getElementById("answer");
-let solution = num1*num2;
-answer. textContent = solution;
-}
