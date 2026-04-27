15ef1288253ff2f3393f2f02f349376df6b9f0a8
Francis Danaher <francisjd67@gmail.com>
2026-04-27 10:06:55 -0400

    rgb

--- laughingman/laughingman.js
+++ laughingman/laughingman.js
@@ -45,3 +45,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+function newColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);    
+    return 'rgb(${r}, ${g}, ${b})';
+}
