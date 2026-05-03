1142a905a6fa280aae810bb5118d58b15708b9d7
Francis Danaher <francisjd67@gmail.com>
2026-05-02 20:29:39 -0400

    office hour work

--- squares/squares.js
+++ squares/squares.js
@@ -1,12 +1,34 @@
 let squareArea = document.querySelector("#squarearea");
-let squareCount = parseInt(Math. random()*21)+30;
-
+let squareCount = parseInt(Math.random()*21)+30;
+//attach the addSquare function to the button
+document.querySelector("#add").onclick=addSquare;
 for (let i = 0; i < squareCount; i++) {
-
-    let square = document.createElement("div");
-    square. className = "square";
-    square. style. left = parseInt(Math.random()*650) + "px";
-    square. style. top = parseInt (Math. random()*300) + "px";
-
-    squareArea.append (square) ;
+    addSquare();
+    
+    //use the addSquare function instead of the code here to add squares
 }
+function getRandomColor(){
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function addSquare(){
+    //Make this add a single square
+    let square = document.createElement("div");
+    square.className = "square";
+    square.style.left = parseInt(Math.random()*650) + "px";
+    square.style.top = parseInt(Math.random()*300) + "px";
+    square.style.backgroundColor = getRandomColor();
+    //add a click function to each square
+    square.onclick=function(){
+        this.style.backgroundColor = getRandomColor();
+    };
+    squareArea.append(square);
+}
+//write the function to change all colors
+//get all the squares as an array 
+//call the getRandomColor on them to change their colors
