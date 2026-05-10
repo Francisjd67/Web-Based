546cf86d472cff8e8d9d03261cf6051b551eb1cb
Francis Danaher <francisjd67@gmail.com>
2026-05-09 20:46:57 -0400

    fixing 1. and 2

--- laughingman/laughingman.js
+++ laughingman/laughingman.js
@@ -1,59 +1,65 @@
 var SQUARE_COUNT=3;
-const TIMER_SPEED=500
-const SPEED = 20;
+
+const TIMER_SPEED=100
+const SPEED = 10;
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#square").addEventListener("click" , () => {
-        alert("You clicked!");
+        SQUARE_COUNT++;
+        createSquare();
     });
 
     let box = document.querySelector("#box");
 
     for(let i=0; i<SQUARE_COUNT; i++){
-
-        let square = document.createElement('img');
-        square.src = "laughing_man.jpg";
-        square.alt = "Laughing Man";
-        square.className = "square";
-        box.appendChild(square);
-        square.addEventListener("mouseover", (event)=>{
-            event.target.src = "Orochimaru.jpg";
-        });
-        square.addEventListener("mouseout", (event)=>{  
-            event.target.src = "laughing_man.jpg";
-        });
+        createSquare();
+        
     }
 
-    Array.from(box.children).forEach((element) => {
-        const parent = element.parentElement;
-        const maxX = parent.clientWidth - element.clientWidth;
-        const maxY = parent.clientHeight - element.clientHeight;
-
-        let dx = SPEED * (Math.random()*2-1);
-        let dy = SPEED * (Math.random()*2-1);
-
-        let x = parseInt(element.style.left) || 225;
-        let y = parseInt(element.style.top) || 175;
-
-        setInterval(() => { 
-
-            if(x <= 0 || x >= maxX){
-                dx *= -1;
-                element.style.borderColor = newColor();
-            }
-            if(y <= 0 || y >= maxY){
-                dy *= -1;
-                element.style.borderColor = newColor();
-            }
-
-            x += dx;
-            y += dy;
-
-            element.style.left = x + "px";
-            element.style.top = y + "px";
-        }, TIMER_SPEED) 
-    });
-
 });
+
+function createSquare(){
+    let square = document.createElement('img');
+    square.src = "laughing_man.jpg";
+    square.alt = "Laughing Man";
+    square.className = "square";
+    box.appendChild(square);
+    square.addEventListener("mouseover", (event)=>{
+        event.target.src = "Orochimaru.jpg";
+    });
+    square.addEventListener("mouseout", (event)=>{  
+        event.target.src = "laughing_man.jpg";
+    });
+    square.addEventListener("mouseover", (event)=>{
+        event.target.style.backgroundColor = getRandomColor();
+    });
+    const parent = square.parentElement;
+    const maxX = parent.clientWidth - square.clientWidth;
+    const maxY = parent.clientHeight - square.clientHeight;
+
+    let dx = SPEED * (Math.random()*2-1);
+    let dy = SPEED * (Math.random()*2-1);
+
+    let x = parseInt(square.style.left) || 225;
+    let y = parseInt(square.style.top) || 175;
+
+    setInterval(() => { 
+
+        if(x <= 0 || x >= maxX){
+            dx *= -1;
+            square.style.borderColor = newColor();
+        }
+        if(y <= 0 || y >= maxY){
+            dy *= -1;
+            square.style.borderColor = newColor();
+        }
+
+        x += dx;
+        y += dy;
+
+        square.style.left = x + "px";
+        square.style.top = y + "px";
+    }, TIMER_SPEED)
+}
 
 function newColor(){
     let r = Math.floor(Math.random() * 256);
