263ca36a1f54830cdb5f4f5fd58a753bc7ae1a4a
Francis Danaher <francisjd67@gmail.com>
2026-05-09 20:21:05 -0400

    4. have the border change colors when hitting the walls

--- laughingman/laughingman.js
+++ laughingman/laughingman.js
@@ -1,6 +1,6 @@
-const SQUARE_COUNT=3;
+var SQUARE_COUNT=3;
 const TIMER_SPEED=500
-const SPEED = 5;
+const SPEED = 20;
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#square").addEventListener("click" , () => {
         alert("You clicked!");
@@ -11,29 +11,33 @@ document.addEventListener('DOMContentLoaded', () => {
     for(let i=0; i<SQUARE_COUNT; i++){
 
         let square = document.createElement('img');
-         square.src = "laughin_man.jpg";
+         square.src = "laughing_man.jpg";
          square.alt = "Laughing Man";
          square.className = "square";
          box.appendChild(square);
+
     }
 
-    Array.from(box.children).forEach(()=> {
+    Array.from(box.children).forEach((element) => {
         const parent = element.parentElement;
         const maxX = parent.clientWidth - element.clientWidth;
         const maxY = parent.clientHeight - element.clientHeight;
-    
+
         let dx = SPEED * (Math.random()*2-1);
         let dy = SPEED * (Math.random()*2-1);
 
         let x = parseInt(element.style.left) || 225;
         let y = parseInt(element.style.top) || 175;
+
         setInterval(() => { 
 
             if(x <= 0 || x >= maxX){
                 dx *= -1;
+                element.style.borderColor = newColor();
             }
             if(y <= 0 || y >= maxY){
                 dy *= -1;
+                element.style.borderColor = newColor();
             }
 
             x += dx;
@@ -50,5 +54,5 @@ function newColor(){
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);    
-    return 'rgb(${r}, ${g}, ${b})';
+    return `rgb(${r}, ${g}, ${b})`;
 }
